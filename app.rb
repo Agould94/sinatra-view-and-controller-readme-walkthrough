281060cf2289ecb3b6fd18d166cd40ba0f228858
Alexander Gould <alexander.gould94@gmail.com>
2021-04-28 12:48:42 -0700

    commit tree 38567021f48ff6b91354fba5dda8f0978b0ad649

--- app.rb
+++ app.rb
@@ -1,16 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  set :views, settings.root + '/4:26-4:30/sinatra-view-and-controller-readme-walkthrough/views'
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    origional_string = params[:string]
+    @reversed_string = origional_string.reverse
+
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
 
   end
 end
